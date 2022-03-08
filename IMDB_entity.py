96e9cb17925c78adb6830717b939e1daf0b26c98
Kovacs Balint <kbalint96@gmail.com>
2022-03-08 22:06:36 +0100

    List to dict, JSON export, util constants

--- IMDB_entity.py
+++ IMDB_entity.py
@@ -15,46 +15,44 @@ class IMDBEntity:
     def __init__(self, id, data):
         self.id = id
 
-        # here should come a function which parses the data and creates a dict from JSON
-        self.data = self.data_to_dict(data)
-
         # after the data is structured by the previous function, its data should be assigned to class variables
-        self.model_data()
+        self.model_data(data)
 
     def data_to_dict(self, data):
         return json.loads("".join(data))
 
-    def model_data(self):
-        self.parse_rating()
-        self.parse_num_of_ratings()
-        self.parse_num_of_oscars()
-        self.parse_title()
+    def model_data(self, data):
+        data = self.data_to_dict(data)
+        self.parse_rating(data)
+        self.parse_num_of_ratings(data)
+        self.parse_num_of_oscars(data)
+        self.parse_title(data)
 
-    def parse_rating(self):
+    def parse_rating(self, data):
         try:
-            self.rating = self.adjusted_rating = self.data['props']['pageProps']['aboveTheFoldData']['ratingsSummary']['aggregateRating']
+            self.rating = self.adjusted_rating = data['props']['pageProps']['aboveTheFoldData']['ratingsSummary']['aggregateRating']
         except TypeError:
             logging.warning("Could not extract rating from movie #%d data, value is set to %s!", self.id, utils.DEFAULT_DATA_VALUE_INT)
             self.rating = self.adjusted_rating = utils.DEFAULT_DATA_VALUE_INT
 
 
-    def parse_num_of_ratings(self):
+    def parse_num_of_ratings(self, data):
         try:
-            self.num_of_ratings = self.data['props']['pageProps']['aboveTheFoldData']['ratingsSummary']['voteCount']
+            self.num_of_ratings = data['props']['pageProps']['aboveTheFoldData']['ratingsSummary']['voteCount']
         except TypeError:
             logging.warning("Could not extract num_of_ratings from movie #%d data, value is set to %s!", self.id, utils.DEFAULT_DATA_VALUE_INT)
             self.num_of_ratings = utils.DEFAULT_DATA_VALUE_INT
 
-    def parse_num_of_oscars(self):
+    def parse_num_of_oscars(self, data):
         try:
-            self.num_of_oscars = self.data['props']['pageProps']['mainColumnData']['prestigiousAwardSummary']['wins']
+            self.num_of_oscars = data['props']['pageProps']['mainColumnData']['prestigiousAwardSummary']['wins']
         except TypeError:
             logging.warning("Could not extract num_of_oscars from movie #%d data, value is set to %s!", self.id, utils.DEFAULT_DATA_VALUE_INT)
             self.num_of_oscars = utils.DEFAULT_DATA_VALUE_INT
 
-    def parse_title(self):
+    def parse_title(self, data):
         try:
-            self.title = self.data['props']['pageProps']['aboveTheFoldData']['titleText']['text']
+            self.title = data['props']['pageProps']['aboveTheFoldData']['titleText']['text']
         except TypeError:
             logging.warning("Could not extract title from movie #%d data, value is set to %s!", self.id, utils.DEFAULT_DATA_VALUE_STR)
             self.title = utils.DEFAULT_DATA_VALUE_STR
--- utils.py
+++ utils.py
@@ -1,9 +1,24 @@
 import requests as r
+from IMDB_entity import IMDBEntity
+import json
+import time
 
 IMDB_TITLE_COLUMN = "td.titleColumn a"
 IMDB_SOUP_PARSER = "html.parser"
-IMDB_TOP_RESULTS = 1
+IMDB_TOP_RESULTS = 10
+DEFAULT_DATA_VALUE_INT = 0
+DEFAULT_DATA_VALUE_STR = ""
 
 def transform_url(url):
     return r.get(url).text
 
+def list_to_nested_dictionary(entities_list):
+    entities_dict = {}
+    for i in range(len(entities_list)):
+        entities_dict[i] = vars(entities_list[i])
+
+    return entities_dict
+
+def data_to_json(data):
+    with open("imdb_quest" + str(time.time()) + ".json", "w", encoding='UTF-8') as json_file:
+        json.dump(data, json_file)
